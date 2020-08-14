91ad38729e0519810d74d90dd15b5b34798d08c9
leonardov31 <leonardoviana098@gmail.com>
2020-08-14 11:01:20 -0300

    Custom countdown timer created from scratch

---
+++ countdown_timer/lib/components/circular_progress_bar.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class CustomTimerPainter extends CustomPainter {
+  CustomTimerPainter({
+    this.animation,
+    this.backgroundColor,
+    this.color,
+  }) : super(repaint: animation);
+  final Animation<double> animation;
+  final Color backgroundColor, color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = backgroundColor
+      ..strokeWidth = 10.0
+      ..strokeCap = StrokeCap.butt
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
+
+    paint.color = color;
+    double progress = (1.0 - animation.value) * 2 * pi;
+    canvas.drawArc(Offset.zero & size, pi * 1.5, -progress, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomTimerPainter old) {
+    return animation.value != old.animation.value ||
+        color != old.color ||
+        backgroundColor != old.backgroundColor;
+  }
+}
