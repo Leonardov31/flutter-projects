ea540e76d54d1275414ac2d59c8fbf2593d24687
Leonardov31 <leonardoviana098@gmail.com>
2020-08-28 10:34:26 -0300

    dialog to edit list created

---
+++ shopping_list/lib/ui/shopping_list_dialog.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping_list/models/shopping_list.dart';
+import 'package:shopping_list/utils/dbhelper.dart';
+
+class ShoppingListDialog {
+  final txtName = TextEditingController();
+  final txtPriority = TextEditingController();
+
+  Widget buildDialog(BuildContext context, ShoppingList list, bool isNew) {
+    DbHelper helper = DbHelper();
+    if (!isNew) {
+      txtName.text = list.name;
+      txtPriority.text = list.priority.toString();
+    }
+
+    return AlertDialog(
+      title: Text((isNew) ? 'New Shopping list' : 'Edit shopping list'),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      content: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: txtName,
+              decoration: InputDecoration(
+                hintText: 'Shopping List Name',
+              ),
+            ),
+            TextField(
+              controller: txtPriority,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Shopping List Priority (1 - 3)',
+              ),
+            ),
+            RaisedButton(
+              child: Text('Save Shopping List'),
+              onPressed: () {
+                list.name = txtName.text;
+                list.priority = int.parse(txtPriority.text);
+                helper.insertList(list);
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
