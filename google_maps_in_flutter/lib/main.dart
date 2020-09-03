fbf61f16c2490f8a74741c439673744b8f0b180a
Leonardov31 <leonardoviana098@gmail.com>
2020-09-03 11:07:21 -0300

    added buttons to control the map

--- google_maps_in_flutter/lib/main.dart
+++ google_maps_in_flutter/lib/main.dart
@@ -26,6 +26,7 @@ class MapsView extends StatefulWidget {
 class _MapsViewState extends State<MapsView> {
   CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
   GoogleMapController mapController;
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -46,6 +47,71 @@ class _MapsViewState extends State<MapsView> {
               onMapCreated: (GoogleMapController controller) {
                 mapController = controller;
               },
+            ),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10, bottom: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ClipOval(
+                        child: Material(
+                          color: Colors.pink[100],
+                          child: InkWell(
+                            splashColor: Colors.pink[500],
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Icon(Icons.add),
+                            ),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.zoomIn(),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      ClipOval(
+                        child: Material(
+                          color: Colors.pink[100],
+                          child: InkWell(
+                            splashColor: Colors.pink[500],
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Icon(Icons.remove),
+                            ),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.zoomOut(),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      ClipOval(
+                        child: Material(
+                          color: Colors.pink[100],
+                          child: InkWell(
+                            splashColor: Colors.pink[500],
+                            child: SizedBox(
+                              width: 56,
+                              height: 56,
+                              child: Icon(Icons.my_location),
+                            ),
+                            onTap: () {},
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
