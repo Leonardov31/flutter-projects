21c65b914ae739121805f4b68be3d8754c82d506
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2020-12-09 09:59:56 -0300

    updated path

--- flash-chat-flutter/ios/Flutter/flutter_export_environment.sh
+++ flash-chat-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\leona\Documents\flutter-projects\flash-chat-flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\leona\Documents\GitHub\flutter-projects\flash-chat-flutter"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
